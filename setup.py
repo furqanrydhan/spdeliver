1e4d8c071621a4fdc4b3ca0752608f4581140d9d
Matt Bornski <matt@bornski.com>
2011-09-13 16:39:46 -0700

    Report version

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os.path
 import setuptools
  
 def setup():
+    with open(os.path.join('src', 'spdeliver.py'), 'r') as f:
+        for line in f.readlines():
+            if 'version' in line:
+                try:
+                    exec(line)
+                    assert(isinstance(version, basestring))
+                    break
+                except (SyntaxError, AssertionError, NameError):
+                    pass
+    try:
+        assert(isinstance(version, basestring))
+    except (AssertionError, NameError):
+        version = 'unknown'
+    
     setuptools.setup(
         name='spdeliver',
-        version='0.1',
+        version=version,
         description='StylePage tools: Python message delivery',
         author='mattbornski',
         url='http://github.com/stylepage/spdeliver',
@@ -14,15 +29,14 @@ def setup():
         py_modules=[
             'spdeliver',
         ],
-        dependency_links=[
-            'http://github.com/facebook/python-sdk/tarball/master#egg=facebook-python-sdk',
-        ],
         install_requires=[
             'facebook-python-sdk',
             'python-twitter',
             'oauth2',
             'gdata',
-#            'APNSWrapper',
+        ],
+        dependency_links=[
+            'http://github.com/facebook/python-sdk/tarball/master#egg=facebook-python-sdk',
         ],
     )
 
